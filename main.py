5848c85bab1cf57d3e19211a53c96ec602c0b3c4
imoQ <526flatter@gmail.com>
2019-05-02 22:19:08 +0900

    ディレクトリ構成変更

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 
 app = Flask(__name__,
-            #static_folder = './static',
-            template_folder = './static')
+            static_folder = './static',
+            template_folder = './templates')
 
 @app.route('/')
 def initapp():
     return render_template('index.html')
 
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
 
 if __name__ == '__main__':
-    pass
+    app.run()
