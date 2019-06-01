fe1c37000eb69f5ab071f40dc7d4e8e3218f0f41
imoQ <526flatter@gmail.com>
2019-06-01 18:28:13 +0900

    ちょっとだけリファクタ

--- static/js/articles.js
+++ static/js/articles.js
@@ -1,4 +1,3 @@
-var app = null;
 var axs = axios.create({
     baseURL: 'http://localhost:5000', // バックエンドB のURL:port を指定する
     headers: {
@@ -8,41 +7,35 @@ var axs = axios.create({
     responseType: 'json'  
 })
 
-const initapp = () => {
-    app = new Vue({
-        el: '#app',
-        data : {
-            articles : [],
-            article_count : 0
-        },
-        mounted : async function(){
-            let url = CONTEXT_PATH + 'articles/getFirstArticles/'
-            console.log(url)
-            let response = await axios.get(url)
-            console.log(response)
+var app = new Vue({
+    el: '#app',
+    data : {
+        articles : [],
+        article_count : 0
+    },
+    mounted : async function(){
+        let url = CONTEXT_PATH + 'articles/getFirstArticles/'
+        console.log(url)
+        let response = await axios.get(url)
+        console.log(response)
+        app.article_count = response.data.article_count
+        app.articles = response.data.articles
+    },
+    methods : {
+        getNextArticles: async function(){
+            let length = app.articles.length
+            let lastid = app.articles[length-1].id
+        
+            let url = CONTEXT_PATH + 'articles/getNextArticles/'
+            // let param = new URLSearchParams();
+            // param.append('lastid', lastid)
+            let param = {
+                lastid: lastid
+            }
+
+            let response = await axs.post(url, param)
             app.article_count = response.data.article_count
             app.articles = response.data.articles
-        },
-        methods : {
-            getNextArticles: async function(){
-                let length = app.articles.length
-                let lastid = app.articles[length-1].id
-            
-                let url = CONTEXT_PATH + 'articles/getNextArticles/'
-                // let param = new URLSearchParams();
-                // param.append('lastid', lastid)
-                let param = {
-                    lastid: lastid
-                }
-
-                let response = await axs.post(url, param)
-                app.article_count = response.data.article_count
-                app.articles = response.data.articles
-            }
         }
-    });
-    
-}
-
-
-initapp();
+    }
+});
