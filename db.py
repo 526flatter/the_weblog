b245ff89164acd072f5e2ebcc7cc87cfdadb77c3
imoQ <526flatter@gmail.com>
2019-06-09 13:38:42 +0900

    コネクションクローズ関数追加

--- db.py
+++ db.py
@@ -1,10 +1,19 @@
 import sqlite3
 from flask import g
 from contextlib import closing
-
-DATABASE = './test_db.sqlite3'
+import config.config as conf
 
 def connect_db():
-    return sqlite3.connect(DATABASE)
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            conf.DB,
+            detect_types = sqlite3.PARSE_DECLTYPES
+            )
+        g.db.row_factory = sqlite3.Row
+    return g.db
 
+def close_db():
+    db = g.pop('db', None)
 
+    if db is not None:
+        db.close()
